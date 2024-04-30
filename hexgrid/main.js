e4a9d2d9da3289c78322f7597075d66a8d2c212d
MarkyMan4 <markus.hutnik@gmail.com>
2024-04-29 22:56:23 -0500

    window resizing

--- hexgrid/main.js
+++ hexgrid/main.js
@@ -4,14 +4,20 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let gridOffsetX = canvas.width / 2;
+let gridOffsetY = canvas.height / 2;
+let hexagons = [];
+
+const hexRadius = 50;
+
 window.addEventListener("resize", _ => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-});
+    gridOffsetX = canvas.width / 2;
+    gridOffsetY = canvas.height / 2;   
 
-const hexRadius = 50;
-const gridOffsetX = canvas.width / 2;
-const gridOffsetY = canvas.height / 2;
+    generateAndDrawHexagons();
+});
 
 const drawHexagon = (hex) => {
     ctx.translate(hex.x, hex.y);
@@ -46,37 +52,30 @@ const drawHexagon = (hex) => {
     ctx.resetTransform();
 }
 
-let hexagons = [];
+const generateAndDrawHexagons = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-// should be a better way to do this
-// goal is to find all combinations of q, r and s such that q + r + s = 0
-for(let q = -3; q <= 3; q++) {
-    for(let r = -3; r <= 3; r++) {
-        for(let s = -3; s <= 3; s++) {
-            // yuck?
-            if(q + r + s === 0) {
-                // hex to pixel calculation - https://www.redblobgames.com/grids/hexagons/#hex-to-pixel
-                let x = hexRadius * ((3/2) * q) + gridOffsetX;
-                let y = hexRadius * ((Math.sqrt(3) / 2) * q + Math.sqrt(3) * r) + gridOffsetY;
-                hexagons.push({q: q, r: r, s: s, x: x, y: y});
+    hexagons = [];
+
+    // should be a better way to do this
+    // goal is to find all combinations of q, r and s such that q + r + s = 0
+    for(let q = -3; q <= 3; q++) {
+        for(let r = -3; r <= 3; r++) {
+            for(let s = -3; s <= 3; s++) {
+                // yuck?
+                if(q + r + s === 0) {
+                    // hex to pixel calculation - https://www.redblobgames.com/grids/hexagons/#hex-to-pixel
+                    let x = hexRadius * ((3/2) * q) + gridOffsetX;
+                    let y = hexRadius * ((Math.sqrt(3) / 2) * q + Math.sqrt(3) * r) + gridOffsetY;
+                    hexagons.push({q: q, r: r, s: s, x: x, y: y});
+                }
             }
         }
     }
+
+    hexagons.forEach(hex => {
+        drawHexagon(hex);
+    });
 }
 
-hexagons.forEach(hex => {
-    drawHexagon(hex);
-});
-
-
-// const animate = () => {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//     hexagons.forEach(hex => {
-//         drawHexagon(hex);
-//     });
-
-//     requestAnimationFrame(animate);
-// }
-
-// animate();
+generateAndDrawHexagons();

5c3b0b1328126be186c64528ea27150c6f19f83e
MarkyMan4 <markus.hutnik@gmail.com>
2024-10-17 21:35:15 -0500

    turtle: canvas scaling, draw turtle

--- turtle/main.js
+++ turtle/main.js
@@ -1,2 +1,15 @@
+const canvasContainer = document.getElementById("canvas-container");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+canvas.width = canvasContainer.getBoundingClientRect().width;
+canvas.height = canvasContainer.getBoundingClientRect().height;
+
+// draw the turtle
+ctx.beginPath();
+ctx.moveTo(100, 100);
+ctx.lineTo(90, 90);
+ctx.lineTo(90, 110);
+ctx.lineTo(100, 100);
+ctx.strokeStyle = "white";
+ctx.stroke();

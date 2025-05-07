e9310e1ded408fa41ee91610342d473a2efedd85
MarkyMan4 <markus.hutnik@gmail.com>
2025-05-07 00:27:16 -0500

    brookie: animate spots

--- brookie/main.js
+++ brookie/main.js
@@ -9,8 +9,8 @@ class Spot {
         this.x = x;
         this.y = y;
         this.radius = (Math.random() * 10) + 10;
-        this.xVel = Math.random() * -5;
-        this.hasHalo = Math.random() < 0.1;
+        this.xVel = Math.random() * 5 + 1;
+        this.hasHalo = Math.random() < 0.2;
     }
 
     drawCircle(ctx, x, y, r, color) {
@@ -31,23 +31,45 @@ class Spot {
     }
 
     update() { 
-
+        this.x += this.xVel;
+        this.y = (Math.sin(this.x / 200) * 2) + this.y;
     }
 }
 
 let spots = [];
 
-for(let i = 0; i < 50; i++) {
-    spots.push(new Spot(Math.random() * canvas.width, Math.random() * canvas.height));
+function spawnSpots() {
+    for(let i = 0; i < 30; i++) {
+        spots.push(new Spot(-200, Math.random() * (canvas.height - (canvas.height / 3))));
+    }
 }
+
+// initial spawn
+spawnSpots();
+
+// spawn 10 spots every second
+setInterval(() => {
+    spawnSpots();
+}, 2000);
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    spots.forEach(s => {
+    spotIndiciesToRemove = [];
+
+    spots.forEach((s, idx) => {
         s.draw(ctx);
         s.update();
+
+        if(s.x - s.radius > canvas.width) {
+            spotIndiciesToRemove.push(idx);
+        }
     });
+
+    // remove off screen spots
+    for(let i = spotIndiciesToRemove.length - 1; i >= 0; i--) {
+        spots.splice(spotIndiciesToRemove[i], 1);
+    }
 
     setTimeout(() => requestAnimationFrame(animate), 60 / 1000);
 }
